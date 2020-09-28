cfd80e0216d0af04e30cd3f925734756e2e3b2c5
Lea Kapelevich <leakapelevich@gmail.com>
2020-09-28 10:50:37 -0400

    use intercept from glmnetjl

--- experiments/houses.jl
+++ experiments/houses.jl
@@ -18,6 +18,7 @@ gamma_range = [1e-7, 1e-6, 1e-4, 0.001, 0.03, 0.01, 0.1, 1.0, 10.0, 100.0]
 q_range = 1:10
 silent = true
 depths = 0:5
+seeds = 1:5
 
 # allow clusters to be an input in case of empty clusters
 function make_clusters(X_list, Y_list, memberships_list, clusters)
@@ -66,9 +67,13 @@ function train_sparclur(depth, seed; relaxation = true, ignore_coordination = fa
     (num_obs, num_features) = size(X_big_list)
     clusters = unique(memberships_list) # not contiguous
     folds = kfolds(collect(1:num_obs), k = 5)
-    mse_scores = zeros(length(q_range), length(gamma_range))
     valid_io = open("validation_depth$(depth).csv", "w")
     println(valid_io, "fold,q,gamma,mse")
+    if ignore_coordination
+        mse_scores = [zeros(length(q_range), length(gamma_range)) for c in eachindex(clusters)]
+    else
+        mse_scores = zeros(length(q_range), length(gamma_range))
+    end
     for (fold_idx, (train_idxs, valid_idxs)) in enumerate(folds)
         # split data
         X_train_list = view(X_big_list, train_idxs, :)
@@ -81,54 +86,58 @@ function train_sparclur(depth, seed; relaxation = true, ignore_coordination = fa
         (Xs_train, Ys_train) = make_clusters(X_train_list, Y_train_list, memberships_train, clusters)
         (Xs_valid, Ys_valid) = make_clusters(X_valid_list, Y_valid_list, memberships_valid, clusters)
         # grid
-        for (gamma_idx, gamma) in enumerate(gamma_range)
-            for (q_idx, q) in enumerate(q_range)
-                if relaxation
-                    if ignore_coordination
-                        supp = Vector{Int}[]
-                        weights = Vector{Float64}[]
-                        # one leaf = single cluster at a time
-                        for c in eachindex(clusters)
-                            (supp_c, num_supp, weights_c) = SparClur2.solve_relaxation([Xs_train[c]], [Ys_train[c]], q, gamma = gamma)
-                            push!(supp, supp_c[1:num_supp])
-                            push!(weights, weights_c[1])
-                        end
-                    else
-                        (supp_c, _, weights) = SparClur2.solve_relaxation(Xs_train, Ys_train, q, gamma = gamma)
-                        # repeat the same support for all clusters (tidy this line)
-                        supp = fill(supp_c, length(clusters))
+        for (gamma_idx, gamma) in enumerate(gamma_range), (q_idx, q) in enumerate(q_range)
+            if relaxation
+                if ignore_coordination
+                    # one leaf = single cluster at a time
+                    for c in eachindex(clusters)
+                        (supp, num_supp, weights) = SparClur2.solve_relaxation([Xs_train[c]], [Ys_train[c]], q, gamma = gamma)
+                        Y_pred = Xs_valid[c][:, supp[1:num_supp]] * weights[1]
+                        mse_scores[c][q_idx, gamma_idx] += sum(abs2, Y_pred - Ys_valid[c])
+                        println(valid_io, "$(fold_idx),$(q),$(gamma),$(mse_scores[c][q_idx, gamma_idx])")
                     end
                 else
-                    if ignore_coordination
-                        supp = Vector{Int}[]
-                        weights = Vector{Float64}[]
-                        # one leaf = single cluster at a time
-                        for c in eachindex(clusters)
-                            warm_start = get_warm_start([Xs_train[c]], [Ys_train[c]], q)
-                            (supp_c, weights_c) = SparClur2.solve_MIOP([Xs_train[c]], [Ys_train[c]], q, gamma, optimizer, silent = silent, optimizer_params = optimizer_params, bin_init = warm_start)
-                            push!(supp, supp_c)
-                            push!(weights, weights_c[1])
-                        end
-                    else
-                        warm_start = get_warm_start(Xs_train, Ys_train, q)
-                        (supp_c, weights) = SparClur2.solve_MIOP(Xs_train, Ys_train, q, gamma, optimizer, silent = silent, optimizer_params = optimizer_params, bin_init = warm_start)
-                        # repeat the same support for all clusters (tidy this line)
-                        supp = fill(supp_c, length(clusters))
-                    end
+                    (supp, num_supp, weights) = SparClur2.solve_relaxation(Xs_train, Ys_train, q, gamma = gamma)
+                    Ys_pred = [Xs_valid[c][:, supp] * weights[c] for c in eachindex(clusters)]
+                    mse_scores[q_idx, gamma_idx] += sum(sum(abs2, Ys_pred[c] - Ys_valid[c]) for c in eachindex(clusters))
+                    println(valid_io, "$(fold_idx),$(q),$(gamma),$(mse_scores[q_idx, gamma_idx])")
                 end
-                Ys_pred = [Xs_valid[c][:, supp[c]] * weights[c] for c in eachindex(clusters)]
-                score = sum(sum(abs2, Ys_pred[c] - Ys_valid[c]) for c in eachindex(clusters))
-                mse_scores[q_idx, gamma_idx] += sum(sum(abs2, Ys_pred[c] - Ys_valid[c]) for c in eachindex(clusters))
-                println(valid_io, "$(fold_idx),$(q),$(gamma),$(mse_scores[q_idx, gamma_idx])")
-                flush(valid_io)
+            else
+                if ignore_coordination
+                    # one leaf = single cluster at a time
+                    for c in eachindex(clusters)
+                        warm_start = get_warm_start([Xs_train[c]], [Ys_train[c]], q)
+                        (supp, weights_c) = SparClur2.solve_MIOP([Xs_train[c]], [Ys_train[c]], q, gamma, optimizer, silent = silent, optimizer_params = optimizer_params, bin_init = warm_start)
+                        Y_pred = Xs_valid[c][:, supp] * weights_c[1]
+                        mse_scores[c][q_idx, gamma_idx] += sum(abs2, Y_pred - Ys_valid[c])
+                        println(valid_io, "$(fold_idx),$(q),$(gamma),$(mse_scores[c][q_idx, gamma_idx])")
+                    end
+                else
+                    warm_start = get_warm_start(Xs_train, Ys_train, q)
+                    (supp, weights) = SparClur2.solve_MIOP(Xs_train, Ys_train, q, gamma, optimizer, silent = silent, optimizer_params = optimizer_params, bin_init = warm_start)
+                    Ys_pred = [Xs_valid[c][:, supp] * weights[c] for c in eachindex(clusters)]
+                    mse_scores[q_idx, gamma_idx] += sum(sum(abs2, Ys_pred[c] - Ys_valid[c]) for c in eachindex(clusters))
+                    println(valid_io, "$(fold_idx),$(q),$(gamma),$(mse_scores[q_idx, gamma_idx])")
+                end
             end
-        end
-    end
+        end # relaxation
+        flush(valid_io)
+    end # gamma, q
     close(valid_io)
 
-    (_, best_idx) = findmin(mse_scores)
-    (best_q, best_gamma) = (q_range[best_idx[1]], gamma_range[best_idx[2]])
-    @show best_q
+    if ignore_coordination
+        # q and gamma are chosen independently for each leaf
+        best_gamma = zeros(length(clusters))
+        best_q = zeros(Int, length(clusters))
+        for c in eachindex(clusters)
+            (_, best_idx) = findmin(mse_scores[c])
+            (best_q[c], best_gamma[c]) = (q_range[best_idx[1]], gamma_range[best_idx[2]])
+        end
+    else
+        (_, best_idx) = findmin(mse_scores)
+        (best_q, best_gamma) = (q_range[best_idx[1]], gamma_range[best_idx[2]])
+    end
+
     # retrain
     (Xs_big, Ys_big) = make_clusters(X_big_list, Y_big_list, memberships_list, clusters)
     if relaxation
@@ -178,7 +187,7 @@ end
 
 function test_sparclur(; ignore_coord = false, use_relaxation = true)
     res = zeros(length(depths))
-    for (depth_idx, depth) in enumerate(depths), seed in 1:5
+    for (depth_idx, depth) in enumerate(depths), seed in seeds
         # (clusters, best_gamma, (supp, weights)) = sol
         (clusters, best_gamma, (supp, weights)) = train_sparclur(depth, seed, relaxation = use_relaxation, ignore_coordination = ignore_coord)
         (X_list, Y_list, memberships_list) = read_data(depth, seed, false)
@@ -206,16 +215,16 @@ end
 # end
 
 function sparclur_r2()
-    for use_relaxation in [true], ignore_coord in [false], depth in depths
+    for use_relaxation in [true], ignore_coord in [true], depth in depths
         r2 = 0.0
-        for seed in 1:5
+        for seed in seeds
             open("output/housing_depth_$(depth)_ignore_coord_$(ignore_coord)_relaxation_$(use_relaxation)_s$(seed).txt", "r") do io
                 readline(io)
                 readline(io)
                 r2 += parse(Float64, readline(io))
             end
         end
-        @show use_relaxation, ignore_coord, depth, round(r2 / 5, digits = 3)
+        @show use_relaxation, ignore_coord, depth, round(r2 / length(seeds), digits = 3)
     end
 end
 # sparclur_r2()
@@ -241,48 +250,75 @@ function vars_per_leaf(supports)
 end
 
 function sparclur_vars()
-    for use_relaxation in [true], ignore_coord in [true], depth in depths
-        (min_leaf, max_leaf, mean_leaf, often, seldom) = ([], [], [], [], [])
-        for seed in 1:5
+    depth = 5
+    for use_relaxation in [false], ignore_coord in [false]
+        (min_leaf, max_leaf, mean_leaf, often, seldom) = (0.0, 0.0, 0.0, 0.0, 0.0)
+        for seed in seeds
             open("output/housing_depth_$(depth)_ignore_coord_$(ignore_coord)_relaxation_$(use_relaxation)_s$(seed).txt", "r") do io
                 supp_str = readline(io)
                 supports = eval(Meta.parse(supp_str))
+                weights_str = readline(io)
+                weights = eval(Meta.parse(weights_str))
+                for i in eachindex(supports)
+                    # don't count variables that would be thrown out in a tree
+                    supports[i] = supports[i][abs.(weights[i]) .> 1e-5]
+                end
                 (min_leaf_s, max_leaf_s, mean_leaf_s, often_s, seldom_s) = vars_per_leaf(supports)
-                push!(min_leaf, min_leaf_s)
-                push!(max_leaf, max_leaf_s)
-                push!(mean_leaf, mean_leaf_s)
-                push!(often, often_s)
-                push!(seldom, seldom_s)
+                min_leaf += min_leaf_s / length(seeds)
+                max_leaf += max_leaf_s / length(seeds)
+                mean_leaf += mean_leaf_s / length(seeds)
+                often += often_s / length(seeds)
+                seldom += seldom_s / length(seeds)
             end
         end
-        println(round(mean(min_leaf), digits = 3), " ", round(mean(max_leaf), digits = 3), " ", round(mean(mean_leaf), digits = 3), " ", round(mean(often), digits = 3), " ", round(mean(seldom), digits = 3))
+        println(round(min_leaf, digits = 3), " ", round(max_leaf, digits = 3), " ", round(mean_leaf, digits = 3), " ", round(often, digits = 3), " ", round(seldom, digits = 3))
     end
 end
 
 function test_lasso()
     res = zeros(length(depths))
-    for (depth_idx, depth) in enumerate(depths), seed in 1:5
+    supports = Vector[]
+    for (depth_idx, depth) in enumerate(depths), seed in seeds
         (X_train_list, Y_train_list, memberships_train) = read_data(depth, seed, true)
-        (X_test_list, Y_test_list, memberships_test) = read_data(depth, seed, true)
+        (X_test_list, Y_test_list, memberships_test) = read_data(depth, seed, false)
         clusters = unique(memberships_train)
         (Xs_train, Ys_train) = make_clusters(X_train_list, Y_train_list, memberships_train, clusters)
         (Xs_test, Ys_test) = make_clusters(X_test_list, Y_test_list, memberships_test, clusters)
+        num_features = size(X_train_list, 2)
         mse = 0.0
         for c in eachindex(clusters)
-            cv = glmnetcv(Xs_train[c], Ys_train[c])
-            Y_pred = Xs_test[c] * cv.path.betas[:, argmin(cv.meanloss)]
+            # allow lasso not to penalize intercept
+            cv = glmnetcv(Xs_train[c][:, 2:end], Ys_train[c], intercept = true)
+            best_gamma = argmin(cv.meanloss)
+            Y_pred = Xs_test[c][:, 2:end] * cv.path.betas[:, best_gamma] .+ cv.path.a0[best_gamma]
             mse += sum(abs2, Y_pred - Ys_test[c])
+            # record support for variable comparisons at depth 5
+            if depth == 5
+                push!(supports, vcat(findall(cv.path.betas[:, best_gamma] .> 1e-5), num_features + 1))
+            end
         end
         baseline_mse = sum(abs2, Y_test_list .- mean(Y_test_list))
         res[depth_idx] += 1 - mse / baseline_mse
     end
-    return res ./ length(seeds)
+
+    (min_leaf, max_leaf, mean_leaf, often, seldom) = (0.0, 0.0, 0.0, 0.0, 0.0)
+    for seed in seeds
+        (min_leaf_s, max_leaf_s, mean_leaf_s, often_s, seldom_s) = vars_per_leaf(supports)
+        min_leaf += min_leaf_s / length(seeds)
+        max_leaf += max_leaf_s / length(seeds)
+        mean_leaf += mean_leaf_s / length(seeds)
+        often += often_s / length(seeds)
+        seldom += seldom_s / length(seeds)
+    end
+    println(round(min_leaf, digits = 3), " ", round(max_leaf, digits = 3), " ", round(mean_leaf, digits = 3), " ", round(often, digits = 3), " ", round(seldom, digits = 3))
+
+    return round.(res ./ length(seeds), digits = 3)
 end
-# @show round.(test_lasso(), digits = 3)
+@show round.(test_lasso(), digits = 3)
 
 function test_ort_point()
     res = zeros(length(depths))
-    for (depth_idx, depth) in enumerate(depths), seed in 1:5
+    for (depth_idx, depth) in enumerate(depths), seed in seeds
         data_test = Array(CSV.read(joinpath(data_dir, "const_depth$(depth)_test_s$(seed).csv"), DataFrame))
         Y_pred = data_test[:, end - 1]
         Y_test = data_test[:, end - 2]
@@ -296,7 +332,7 @@ end
 #
 function test_ort_linear()
     res = zeros(length(depths))
-    for (depth_idx, depth) in enumerate(depths), seed in 1:5
+    for (depth_idx, depth) in enumerate(depths), seed in seeds
         data_test = Array(CSV.read(joinpath(data_dir, "linear_depth$(depth)_test_s$(seed).csv"), DataFrame))
         Y_pred = data_test[:, end - 1]
         Y_test = data_test[:, end - 2]
